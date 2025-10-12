63ae12985dbbbdb0cd542d1776d3322ee2a2d0fa
Gabor Szabo <gabor@szabgab.com>
2025-10-12 16:45:09 +0300

    update

--- books/python-async/src/examples/async/sleep_loop_async.py
+++ books/python-async/src/examples/async/sleep_loop_async.py
@@ -1,18 +1,27 @@
 import time
+import sys
 import asyncio
 
-async def sleep(cnt, sec):
-    print(f"Start {cnt}")
+async def do_task(task_id: int, sec: int):
+    print(f"Start {task_id}")
     await asyncio.sleep(sec)
-    print(f"End {cnt}")
+    print(f"End {task_id}")
 
 async def main():
+    print("Main started")
+    if len(sys.argv) != 2:
+        exit(f"Usage {sys.argv[0]} NUMBER")
+
     co_routines = []
-    for i in range(4):
-        co_routines.append(sleep(i, 1))
+    for i in range(int(sys.argv[1])):
+        co_routines.append(do_task(i, 1))
+
+    print("Tasks created")
 
     for t in co_routines:
         await t
+
+    print("Main ended")
 
 start = time.monotonic()
 asyncio.run(main())
