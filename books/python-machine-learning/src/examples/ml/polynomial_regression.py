f346da8ce4bb13ebff32ef3ad7fbcd6edd22c080
Gabor Szabo <gabor@szabgab.com>
2026-01-04 12:17:43 +0200

    update

--- books/python-machine-learning/src/examples/ml/polynomial_regression.py
+++ books/python-machine-learning/src/examples/ml/polynomial_regression.py
@@ -1,31 +1,42 @@
 
+# !pip install numpy scikit-learn matplotlib joblib
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+
+
 
 # Generate the input data uisng random numbers
-size = 20
-x = np.random.randint(1, 100, size=size)
-error = np.random.rand(size)
-#error = np.zeros(size)
-y = x * x + error
-#print(error)
-#print(x)
-#print(y)
+def generate_data(size):
+    x = np.random.randint(1, 100, size=size)
+    error = np.random.rand(size)
+    y = x * x + error
+    print(error)
+    #print(x)
+    #print(y)
 
-X = x.reshape((-1, 1))
-#print(X)
-
-transformer = PolynomialFeatures(degree=2, include_bias=False)
-transformer.fit(X)
-X = transformer.transform(X)
-# X = PolynomialFeatures(degree=2, include_bias=False).fit_transform(X)
-#print(X)
+    X = x.reshape((-1, 1))
+    #print(X)
+    return X, y
 
 
-model = LinearRegression().fit(X, y)
-r_sq = model.score(X, y)
-print('coefficient of determination:', r_sq)
-print('intercept:', model.intercept_)
-print('coefficients:', model.coef_)
+#transformer = PolynomialFeatures(degree=2, include_bias=False)
+#transformer.fit(X)
+#X = transformer.transform(X)
+## X = PolynomialFeatures(degree=2, include_bias=False).fit_transform(X)
+##print(X)
+#
+#
+#model = LinearRegression().fit(X, y)
+#r_sq = model.score(X, y)
+#print('coefficient of determination:', r_sq)
+#print('intercept:', model.intercept_)
+#print('coefficients:', model.coef_)
 
+def main():
+    size = 300
+    X, y = generate_data(size)
+    print(len(y))
+
+main()
--- books/python-machine-learning/src/machine-learning2/ml-polynomial-regression.md
+++ books/python-machine-learning/src/machine-learning2/ml-polynomial-regression.md
@@ -1,6 +1,10 @@
 # Polynomial Regression
 
-* When the allow for a function like `a + bx + cx^2 + dx^4 ...` (given a single feature x)
+* When we allow for a function like `a + bx + cx^2 + dx^4 ...` (given a single feature x)
+
+```
+polynomial_regression.ipynb
+```
 
 {% embed include file="src/examples/ml/polynomial_regression.py" %}
 
