d093ea0ebd3be01364d6fe1408d065ed14d11088
Gabor Szabo <gabor@szabgab.com>
2026-03-21 20:12:43 +0100

    setup testing the flask examples

---
+++ books/python-flask/run_tests.pl
@@ -0,0 +1,33 @@
+use strict;
+use warnings;
+use feature 'say';
+use Cwd;
+
+
+my $folder = "src/examples/flask";
+opendir my $dh, $folder or die;
+my @folders = grep {$_ ne "." and $_ ne ".." and not -f "$folder/$_"} readdir $dh;
+close $dh;
+
+my $root = Cwd::cwd();
+#say $root;
+my $counter = 0;
+my $total = @folders;
+for my $dir (@folders) {
+    next if $dir eq 'api_paremeters_required';
+    next if $dir eq 'simple_auth';
+    next if $dir eq 'hello-in-module';
+    next if $dir eq 'color';
+    next if $dir eq 'login';
+    next if $dir eq 'calc-path';
+    next if $dir eq 'st';
+    next if $dir eq '404';
+    next if $dir eq 'api_paremeters';
+    say $dir;
+
+    chdir "$folder/$dir";
+    my $exit_code = system "pytest";
+    chdir $root;
+    die "Test failed in $folder/$dir failed. $counter of $total examples passed." if $exit_code;
+    $counter++;
+}
