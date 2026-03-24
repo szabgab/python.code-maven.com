f4c83ff229fe69de982583b85f72b2194f3e8b56
Gabor Szabo <gabor@szabgab.com>
2026-03-24 07:55:12 +0100

    show html pathes as well

--- books/python-flask/custom.js
+++ books/python-flask/custom.js
@@ -1,4 +1,4 @@
 const prefixes = ["src/examples/"];
-const extensions = [".py", ".toml", ".yaml", ".cfg"];
+const extensions = [".py", ".toml", ".yaml", ".cfg", ".html"];
 const base_url = "https://github.com/szabgab/python.code-maven.com/tree/main/books/python-flask/"
 
