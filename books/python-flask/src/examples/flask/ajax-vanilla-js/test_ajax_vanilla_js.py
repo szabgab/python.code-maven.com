60b482b27cc0267fea89fb9c117a94e44b414295
Gabor Szabo <gabor@szabgab.com>
2026-03-29 08:15:18 +0200

    separate test cases

--- books/python-flask/src/examples/flask/ajax-vanilla-js/test_ajax_vanilla_js.py
+++ books/python-flask/src/examples/flask/ajax-vanilla-js/test_ajax_vanilla_js.py
@@ -1,13 +1,16 @@
 import ajax_vanilla_js
+import pytest
 
+@pytest.fixture()
+def web():
+    return ajax_vanilla_js.app.test_client()
 
-def test_app():
-    web = ajax_vanilla_js.app.test_client()
-
+def test_main_page(web):
     rv = web.get('/')
     assert rv.status == '200 OK'
     assert b'<button id="calc">Calc</button>' in rv.data
 
+def test_api_info(web):
     rv = web.get('/api/info')
     assert rv.status == '200 OK'
     #print(rv.data) # the raw json data
@@ -20,6 +23,7 @@ def test_app():
        "load" : [ 3.21, 7, 14 ]
     }
 
+def test_api_calc(web):
     rv = web.get('/api/calc?a=7&b=8')
     assert rv.status == '200 OK'
     assert rv.headers['Content-Type'] == 'application/json'
@@ -33,6 +37,7 @@ def test_app():
         "divide"   :  0.875,
     }
 
+def test_api_calc_query_string(web):
     rv = web.get('/api/calc', query_string={ 'a' : '10', 'b': '2' })
     assert rv.status == '200 OK'
     assert rv.headers['Content-Type'] == 'application/json'
