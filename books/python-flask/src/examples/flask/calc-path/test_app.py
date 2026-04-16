eb509c4ec1d33312293e9d059c8adcae55f960a4
Gabor Szabo <gabor@szabgab.com>
2026-04-16 11:33:15 +0200

    use fixture

--- books/python-flask/src/examples/flask/calc-path/test_app.py
+++ books/python-flask/src/examples/flask/calc-path/test_app.py
@@ -1,25 +1,24 @@
-import pytest
 import app
+import pytest
 
-def test_main():
-    web = app.app.test_client()
 
+@pytest.fixture()
+def web():
+    return app.app.test_client()
+
+def test_main(web):
     rv = web.get('/')
     assert rv.status == '200 OK'
     assert 'Main<br>' in rv.data.decode('utf-8')
 
 @pytest.mark.parametrize('uid', ['23', '42'])
-def test_user_number(uid):
-    web = app.app.test_client()
-
+def test_user_number(web, uid):
     rv = web.get(f'/user/{uid}')
     assert rv.status == '200 OK'
     assert rv.data.decode('utf-8') == uid
 
 @pytest.mark.parametrize('uid', ['Joe'])
-def test_user_text(uid):
-    web = app.app.test_client()
-
+def test_user_text(web, uid):
     rv = web.get(f'/user/{uid}')
     assert rv.status == '404 NOT FOUND'
     assert '<title>404 Not Found</title>' in rv.data.decode('utf-8')
