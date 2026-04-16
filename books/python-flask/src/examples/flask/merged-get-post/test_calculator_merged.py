09f93e0477887f6e2e0469ce67e7f8447a359817
Gabor Szabo <gabor@szabgab.com>
2026-04-16 11:19:22 +0200

    use fixture

--- books/python-flask/src/examples/flask/merged-get-post/test_calculator_merged.py
+++ books/python-flask/src/examples/flask/merged-get-post/test_calculator_merged.py
@@ -1,24 +1,22 @@
 import calculator_merged
 import pytest
 
+@pytest.fixture()
+def web():
+    return calculator_merged.app.test_client()
 
-def test_app():
-    web = calculator_merged.app.test_client()
 
+def test_app(web):
     rv = web.get('/')
     assert rv.status == '200 OK'
     assert b'<a href="/calc">calc</a>' == rv.data
 
-def test_calc_get():
-    web = calculator_merged.app.test_client()
-
+def test_calc_get(web):
     rv = web.get('/calc')
     assert rv.status == '200 OK'
     assert b'<form' in rv.data
 
-def test_calc_post():
-    web = calculator_merged.app.test_client()
-
+def test_calc_post(web):
     rv = web.post('/calc', data={'a': 7, 'b': 11})
     assert rv.status == '200 OK'
     assert b'18.0' == rv.data
