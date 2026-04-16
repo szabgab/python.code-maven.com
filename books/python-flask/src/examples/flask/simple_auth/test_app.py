d31c64264854cc08b5dd572084d073d497ac4472
Gabor Szabo <gabor@szabgab.com>
2026-04-16 11:21:50 +0200

    use fixture

--- books/python-flask/src/examples/flask/simple_auth/test_app.py
+++ books/python-flask/src/examples/flask/simple_auth/test_app.py
@@ -1,25 +1,24 @@
 import app
 import base64
+import pytest
 
-def test_app():
-    web = app.app.test_client()
+@pytest.fixture()
+def web():
+    return app.app.test_client()
 
+def test_app(web):
     rv = web.get('/')
     assert rv.status == '200 OK'
     assert rv.data == b'Hello World!'
 
-def test_admin_unauth():
-    web = app.app.test_client()
-
+def test_admin_unauth(web):
     rv = web.get('/admin')
     assert rv.status == '401 UNAUTHORIZED'
     assert rv.data == b'Unauthorized Access'
     assert 'WWW-Authenticate' in rv.headers
     assert rv.headers['WWW-Authenticate'] == 'Basic realm="Authentication Required"'
 
-def test_admin_auth():
-    web = app.app.test_client()
-
+def test_admin_auth(web):
     credentials = base64.b64encode(b'john:nhoj').decode('utf-8')
     rv = web.get('/admin', headers={
             'Authorization': 'Basic ' + credentials
