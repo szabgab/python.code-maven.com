415f534ce388e6c3fe1a76af2c2a21d274871177
Gabor Szabo <gabor@szabgab.com>
2026-04-16 11:17:32 +0200

    use fixture

--- books/python-flask/src/examples/flask/teardown/test_app.py
+++ books/python-flask/src/examples/flask/teardown/test_app.py
@@ -1,22 +1,21 @@
 import app
+import pytest
 
-def test_app():
-    web = app.app.test_client()
+@pytest.fixture()
+def web():
+    return app.app.test_client()
 
+def test_app(web):
     rv = web.get('/')
     assert rv.status == '200 OK'
     assert b'<form method="POST" action="/reverse">'in rv.data
 
-def test_reverse():
-    web = app.app.test_client()
-
+def test_reverse(web):
     rv = web.post('/reverse', data={ "text": "foo bar" })
     assert rv.status == '200 OK'
     assert b'rab oof' == rv.data
 
-def test_divide():
-    web = app.app.test_client()
-
+def test_divide(web):
     rv = web.get('/divide')
     assert rv.status == '500 INTERNAL SERVER ERROR'
     assert b'<title>500 Internal Server Error</title>' in rv.data
