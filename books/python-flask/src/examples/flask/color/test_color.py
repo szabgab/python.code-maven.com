e1615402dd81ba32e60aca6f00ae221362ddc24c
Gabor Szabo <gabor@szabgab.com>
2026-04-16 11:15:41 +0200

    use fixture

--- books/python-flask/src/examples/flask/color/test_color.py
+++ books/python-flask/src/examples/flask/color/test_color.py
@@ -1,23 +1,23 @@
 import color
+import pytest
 
-def test_app():
-    web = color.app.test_client()
+@pytest.fixture()
+def web():
+    return color.app.test_client()
 
+
+def test_app(web):
     rv = web.get('/')
     assert rv.status == '200 OK'
     assert 'background-color: #FFFFFF;' in rv.data.decode('utf-8')
 
 # This does not set the color
-def test_get():
-    web = color.app.test_client()
-
+def test_get(web):
     rv = web.get('/?color=AAAAAA')
     assert rv.status == '200 OK'
     assert 'background-color: #FFFFFF;' in rv.data.decode('utf-8')
 
-def test_post():
-    web = color.app.test_client()
-
+def test_post(web):
     rv = web.post('/', data={'color': 'AAAAAA'})
     assert rv.status == '200 OK'
     assert 'background-color: #AAAAAA;' in rv.data.decode('utf-8')
