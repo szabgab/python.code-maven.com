72ad1c1bd46b66d0ab73c729e20780d29d041c3c
Gabor Szabo <gabor@szabgab.com>
2026-04-16 11:23:10 +0200

    use fixture

--- books/python-flask/src/examples/flask/separated-get-post/test_calculator.py
+++ books/python-flask/src/examples/flask/separated-get-post/test_calculator.py
@@ -1,24 +1,21 @@
 import calculator
 import pytest
 
+@pytest.fixture()
+def web():
+    return calculator.app.test_client()
 
-def test_main_page():
-    web = calculator.app.test_client()
-
+def test_main_page(web):
     rv = web.get('/')
     assert rv.status == '200 OK'
     assert b'<a href="/calc">calc</a>' == rv.data
 
-def test_calculator_get():
-    web = calculator.app.test_client()
-
+def test_calculator_get(web):
     rv = web.get('/calc')
     assert rv.status == '200 OK'
     assert b'<form method="POST" action="/calc"' in rv.data
 
-def test_calculator_post():
-    web = calculator.app.test_client()
-
+def test_calculator_post(web):
     rv = web.post('/calc', data={'a': 7, 'b': 11})
     assert rv.status == '200 OK'
     assert b'18.0' == rv.data
