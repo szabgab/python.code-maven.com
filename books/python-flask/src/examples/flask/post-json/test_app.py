cb537eba5c10c5deabfbbb7421aa159b8d8d6467
Gabor Szabo <gabor@szabgab.com>
2026-04-16 11:26:20 +0200

    use fixture

--- books/python-flask/src/examples/flask/post-json/test_app.py
+++ books/python-flask/src/examples/flask/post-json/test_app.py
@@ -1,16 +1,17 @@
 import app
+import pytest
+
+@pytest.fixture()
+def web():
+    return app.app.test_client()
 
 
-def test_app():
-    web = app.app.test_client()
-
+def test_app(web):
     rv = web.get('/')
     assert rv.status == '200 OK'
     assert b'Post JSON to /api/calc' == rv.data
 
-def test_calc():
-    web = app.app.test_client()
-
+def test_calc(web):
     rv = web.post('/api/calc', json={ 'a' : '10', 'b': '2' })
     assert rv.status == '200 OK'
     assert rv.headers['Content-Type'] == 'application/json'
@@ -24,9 +25,7 @@ def test_calc():
         "divide"   :  5,
     }
 
-def test_bad_request():
-    web = app.app.test_client()
-
+def test_bad_request(web):
     rv = web.post('/api/calc')
     assert rv.status == '415 UNSUPPORTED MEDIA TYPE'
 
