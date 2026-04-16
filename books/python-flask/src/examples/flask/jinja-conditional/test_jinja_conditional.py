db5d240387da5135ce9fddf0194833424762af89
Gabor Szabo <gabor@szabgab.com>
2026-04-16 11:11:49 +0200

    use fixture

--- books/python-flask/src/examples/flask/jinja-conditional/test_jinja_conditional.py
+++ books/python-flask/src/examples/flask/jinja-conditional/test_jinja_conditional.py
@@ -1,8 +1,11 @@
 import jinja_conditional
+import pytest
 
-def test_main_page():
-    web = jinja_conditional.app.test_client()
+@pytest.fixture()
+def web():
+    return jinja_conditional.app.test_client()
 
+def test_main_page(web):
     rv = web.get('/')
     assert rv.status == '200 OK'
     assert b'<form action="/echo" method="POST">' in rv.data
@@ -10,26 +13,20 @@ def test_main_page():
     # This is not nice, but this is how it works now.
     assert b'You did not say anything.' in rv.data
 
-def test_echo_post_param():
-    web = jinja_conditional.app.test_client()
-
+def test_echo_post_param(web):
     rv = web.post('/echo', data={ 'text': 'foo bar' })
     assert rv.status == '200 OK'
     assert b'<form action="/echo" method="POST">' in rv.data
     assert b'You said: <b>foo bar</b>' in rv.data
 
-def test_echo_post_empty_param():
-    web = jinja_conditional.app.test_client()
-
+def test_echo_post_empty_param(web):
     rv = web.post('/echo', data={ 'text': '' })
     assert rv.status == '200 OK'
     assert b'<form action="/echo" method="POST">' in rv.data
     assert b'You said' not in rv.data
     assert b'You did not say anything.' in rv.data
 
-def test_echo_post_no_param():
-    web = jinja_conditional.app.test_client()
-
+def test_echo_post_no_param(web):
     rv = web.post('/echo')
     assert rv.status == '200 OK'
     assert b'<form action="/echo" method="POST">' in rv.data
